0ae9f77c5617de26cf9f53846f88ed2aad103793
kashEcosystem <tranvinh6303@gmail.com>
2026-04-26 22:02:43 +0700

    remove test files

--- test/avoid.py
+++
@@ -1,5 +0,0 @@
-def avoid_obstacle(state):
-    if state.distance < 20:
-         state.move_command = "left"
-    else:
-         state.move_command = "forward"
--- test/sensor_fake.py
+++
@@ -1,3 +0,0 @@
-import random
-def get_distance():
-    return random.randint(5 , 100)
